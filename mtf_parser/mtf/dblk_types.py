59d2c1521075737a394528dde1ac9b6399c1196d
Wilderness Ranger <34528883+WRtux@users.noreply.github.com>
2026-07-25 19:34:01 +0800

    Add more DBLK types (as example)
    Added as more examples for agents to implement remaining types.

--- mtf_parser/mtf/dblk_types.py
+++ mtf_parser/mtf/dblk_types.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Self
 
 from .._utils import Structured
-from .._utils import parse_datetime
+from .._utils import parse_datetime, get_timezone
 from .constants import (
 	KnownDBLK,
 	DB_HDR_SIZE, TAPE_HEADER_SIZE,
@@ -118,3 +118,178 @@ class TapeDBLK(DBLK, Structured):
 		return info_map
 
 assert TapeDBLK._field_struct.size == TAPE_HEADER_SIZE - DB_HDR_SIZE
+
+
+@register_dblk_type
+@dataclass(kw_only=True)
+class DatasetDBLK(DBLK, Structured):
+	type_id = KnownDBLK.MTF_SSET.value
+
+	dataset_attributes: int
+	_password_encryption_algo: int
+	_compression_algo: int
+	software_vendor_id: int
+	dataset_index: int
+	_dataset_name_size: int
+	_dataset_name_offset: int
+	_dataset_description_size: int
+	_dataset_description_offset: int
+	_dataset_password_size: int
+	_dataset_password_offset: int
+	_user_name_size: int
+	_user_name_offset: int
+	physical_address: int
+	_media_write_date: bytes
+	_software_major_version: int
+	_software_minor_version: int
+	_timezone: int
+	_mtf_minor_version: int
+	_media_catalog_version: int
+
+	# TODO: Add properties (strings etc.)
+
+	@property
+	def media_write_datetime(self) -> datetime | None:
+		return parse_datetime(self._media_write_date)
+
+	@property
+	def timezone(self) -> timezone | None:
+		return get_timezone(self._timezone)
+
+	_field_specs = (
+		('dataset_attributes', 'I'),
+		('_password_encryption_algo', 'H'),
+		('_compression_algo', 'H'),
+		('software_vendor_id', 'H'),
+		('dataset_index', 'H'),
+		('_dataset_name_size', 'H'),
+		('_dataset_name_offset', 'H'),
+		('_dataset_description_size', 'H'),
+		('_dataset_description_offset', 'H'),
+		('_dataset_password_size', 'H'),
+		('_dataset_password_offset', 'H'),
+		('_user_name_size', 'H'),
+		('_user_name_offset', 'H'),
+		('physical_address', 'Q'),
+		('_media_write_date', '5s'),
+		('_software_major_version', 'B'),
+		('_software_minor_version', 'B'),
+		('_timezone', 'B'),
+		('_mtf_minor_version', 'B'),
+		('_media_catalog_version', 'B'),
+	)
+
+	_info_specs = (
+		*DBLK._info_specs,
+		('physical_address', 'PBA', 'd'),
+		('dataset_index', 'index', 'd'),
+		('dataset_name', 'name', '!s'),
+		('dataset_description', 'desc', '!s'),
+		('media_write_datetime', 'time', '!s'),
+		('user_name', 'user', '!s'),
+		('software_vendor_id', 'vendor_id', '#06x'),
+		# TODO: Maybe incomplete
+	)
+
+	@classmethod
+	def from_bytes(cls, dblk_data: bytes, header: DBHeader) -> Self:
+		if header.type_id != cls.type_id:
+			raise TypeError
+
+		offset = DB_HDR_SIZE
+		field_map = cls._unpack_field_map(dblk_data, offset)
+		offset += cls._field_struct.size
+
+		return cls(header, dblk_data[offset:], extra_offset=offset, **field_map)
+
+assert DatasetDBLK._field_struct.size == 98 - DB_HDR_SIZE
+
+
+# TODO:
+# VolumeDBLK
+# DirectoryDBLK
+
+@register_dblk_type
+@dataclass(kw_only=True)
+class FileDBLK(DBLK, Structured):
+	type_id = KnownDBLK.MTF_FILE.value
+
+	file_attributes: int
+	_modification_date: bytes
+	_creation_date: bytes
+	_backup_date: bytes
+	_access_date: bytes
+	directory_id: int
+	file_id: int
+	_file_name_size: int
+	_file_name_offset: int
+
+	@property
+	def display_size(self) -> int:
+		return self.header.display_size
+
+	@property
+	def modification_datetime(self) -> datetime | None:
+		return parse_datetime(self._modification_date)
+
+	@property
+	def creation_datetime(self) -> datetime | None:
+		return parse_datetime(self._creation_date)
+
+	@property
+	def backup_datetime(self) -> datetime | None:
+		return parse_datetime(self._backup_date)
+
+	@property
+	def access_datetime(self) -> datetime | None:
+		return parse_datetime(self._access_date)
+
+	@property
+	def file_name_raw(self) -> memoryview | None:
+		return self._var_field(self._file_name_offset, self._file_name_size)
+	@property
+	def file_name(self) -> str | None:
+		return self._str_field(self._file_name_offset, self._file_name_size)
+
+	_field_specs = (
+		('file_attributes', 'I'),
+		('_modification_date', '5s'),
+		('_creation_date', '5s'),
+		('_backup_date', '5s'),
+		('_access_date', '5s'),
+		('directory_id', 'I'),
+		('file_id', 'I'),
+		('_file_name_size', 'H'),
+		('_file_name_offset', 'H'),
+	)
+
+	_info_specs = (
+		('directory_id', 'dir', 'd'),
+		('file_name', 'name', '!s'),
+		('display_size', 'size', ',d'),
+		('modification_datetime', 'mtime', '!s'),
+		('creation_datetime', 'btime', '!s'),
+		('backup_datetime', 'rtime', '!s'),
+		('access_datetime', 'atime', '!s'),
+		# TODO: Maybe incomplete
+	)
+
+	@classmethod
+	def from_bytes(cls, dblk_data: bytes, header: DBHeader) -> Self:
+		if header.type_id != cls.type_id:
+			raise TypeError
+
+		offset = DB_HDR_SIZE
+		field_map = cls._unpack_field_map(dblk_data, offset)
+		offset += cls._field_struct.size
+
+		return cls(header, dblk_data[offset:], extra_offset=offset, **field_map)
+
+assert FileDBLK._field_struct.size == 88 - DB_HDR_SIZE
+
+
+# TODO:
+# DatasetEndDBLK
+# TapeEndMarkerDBLK
+# CorruptionDBLK
+# PaddingDBLK (ESPB, SFMB)
